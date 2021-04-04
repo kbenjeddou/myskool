75008062d82297aa44ff9a6897279d2597fea984
Khaled BEN JEDDOU <khaled.benjeddou@gmail.com>
2021-04-04 22:00:39 +0200

    Initial version of myskool generated by JHipster-7.0.1

---
+++ src/main/java/io/myskool/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package io.myskool.repository;
+
+import io.myskool.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/io/myskool/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package io.myskool.service.dto;
